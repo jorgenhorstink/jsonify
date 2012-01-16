9422b5a93cdfa7963afd89aabf88f22fa19b6f12
Jorgen Horstink <mail@jorgenhorstink.nl>
2012-01-16 21:36:05 +0100

    added an example

---
+++ src/jquery.jsonify-0.1.js
@@ -0,0 +1,116 @@
+(function($, undefined) {
+
+    "use strict";
+
+    var getters = {};
+
+    function jsonify(element, options) {
+
+        var json, root, getPath, getValue;
+
+        root = json = {};
+
+        getPath = (function () {
+            // Performance tuning: If there is no descendent with data-jsonify-array attribute, we can just return the simple jsonify-name
+            if (element.find('[data-jsonify-array]').length > 0) {
+
+                // Usefull for repeatable UI components
+                return function (item) {
+                    var parts = [item.data('jsonify-name')], i, parents;
+
+                    parents = item.parents('[data-jsonify-array-item],[data-jsonify-array]').each(function () {
+
+                        var $this = $(this), item = $this.data('jsonify-array-item'), array = $this.data('jsonify-array');
+
+                        if (item !== undefined) {
+                            i = $this.prevAll('[data-jsonify-array-item]').length;
+                        } else if (array !== undefined) {
+                            parts.push(array + '[' + i + ']');
+                        }
+                    });
+
+                    return parts.reverse().join('.');
+                };
+            } else {
+                return function (item) {
+                    return item.data('jsonify-name');
+                };
+            }
+        }());
+
+        getValue = function (item) {
+            if (item.data('jsonify-getter')) {
+                return getters[item.data('jsonify-getter')].apply(item) || options.getters[item.data('jsonify-getter')].apply(item);
+            } else if (item.is('input[type="checkbox"],input[type="radio"]')) {
+                return item.is(':checked') ? item.val() : null;
+            } else {
+                return item.val();
+            }
+        };
+
+        element.find('[data-jsonify-name]').each(function () {
+
+            var path = getPath($(this)), parts = path.split('.'), i = 0, l = parts.length, part, name, ai, value;
+
+            json = root;
+
+            for (i; i < l; i += 1) {
+                part = parts[i];
+
+                if (i === l - 1) {
+                    value = getValue($(this));
+                } else {
+                    value = {};
+                }
+
+                if (part.substr(-1) === ']') {
+                    name = part.split('[')[0];
+                    // array index
+                    ai = part.match(/([0-9]+)\]$/)[1];
+                } else {
+                    name = part;
+                    ai = null;
+                }
+
+                if (ai === null) {
+                    // no array
+
+                    if (!json[name]) {
+                        json[name] = value;
+                    }
+
+                    json = json[name];
+
+                } else {
+                    // array
+
+                    if (!json[name]) {
+                        json[name] = [];
+                    }
+                    if (!json[name][ai]) {
+                        json[name][ai] = value;
+                    }
+
+                    json = json[name][ai];
+                }
+            }
+        });
+
+        return root;
+    }
+
+    $.fn.jsonify = function (options) {
+
+        var ret = null;
+        this.each(function () {
+            ret = jsonify($(this), options);
+        });
+
+        return ret;
+    };
+
+    $.jsonify = function (options) {
+        $.extend(getters, options.getters || {});
+    };
+
+}(window.jQuery));
